84383f672d08d14984ae2155d6e71375ebb1c538
Dmitrii Ivaniushin <defance@gmail.com>
2019-09-23 12:32:56 +0300

    Color friday bot

---
+++ slack_bots/color_friday.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from slack_bots.common.choicer import RandomChoicer
+from slack_bots.common.client import get_client
+from slack_bots.common.files import load_yaml
+
+APP_DIR = Path('color_friday')
+DATA_DIR = Path('data') / APP_DIR
+
+
+def main(client):
+    config = load_yaml(DATA_DIR / 'config.yaml')
+    choicer = RandomChoicer(APP_DIR / 'colors.txt')
+
+    client.chat_postMessage(
+        channel=config['channel'],
+        text=config['message'].format(color=choicer.select()),
+    )
+
+
+if __name__ == '__main__':
+    main(get_client())
