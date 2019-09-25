b5a44cdea042181c76b21a01e639c1410709becb
defance <defance@gmail.com>
2019-09-25 21:36:04 +0300

    Added bit functionality

--- slack_bots/common/client.py
+++ slack_bots/common/client.py
@@ -3,10 +3,14 @@ import os
 import slack
 
 
-def get_client():
+def get_client(**kwargs):
     """
     Returns authorized slack client.
 
     :return: Slack client.
     """
-    return slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
+    return slack.WebClient(token=os.environ['SLACK_API_TOKEN'], **kwargs)
+
+
+def get_rtm_client():
+    return slack.RTMClient(token=os.environ['SLACK_API_TOKEN'], run_async=True)
--- slack_bots/introducer.py
+++ slack_bots/introducer.py
@@ -1,11 +1,17 @@
 """
 Bot introduces person
 """
-
+import asyncio
+import logging
+import os
+import re
+from asyncio import create_task, ensure_future, get_event_loop
+from collections import defaultdict
 from datetime import datetime
 from pathlib import Path
 
 import yaml
+from slack import RTMClient
 
 from slack_bots.common.choicer import RandomChoicer
 from slack_bots.common.client import get_client
@@ -14,9 +20,79 @@ DATA_DIR = Path('data', 'introducer')
 CACHE_DIR = Path('cache', 'introducer')
 
 
+logger = logging.getLogger(__name__)
+
+
+class IntroducerRTMClient(RTMClient):
+    """We need this client to keep callbacks isolated,
+    so they are actually tied to instance.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._callbacks = defaultdict(list)
+
+    def register(self, event, callback):
+        self._validate_callback(callback)
+        self._callbacks[event].append(callback)
+
+
+class IntroducerBot:
+
+    def __init__(self, state=None, config=None):
+        self.state = state or {}
+        self.config = config or {}
+
+    def handle_message(self, **kwargs):
+        # No thread in payload
+        thread = kwargs['data'].get('thread_ts')
+        if not thread:
+            return
+
+        # No thread in state, or they mismatch
+        expected_thread = self.state.get('thread')
+        if not expected_thread or thread != expected_thread:
+            return
+
+        # No user in payload or they mismatch
+        user = kwargs['data'].get('user')
+        if not user or self.state.get('victim_id') != user:
+            return
+
+        # Post reaction
+        reactions = RandomChoicer('reactions.txt', base_dir=DATA_DIR, cache_dir=CACHE_DIR)
+        kwargs['web_client'].chat_postMessage(
+            channel=self.config['channel'],
+            text=reactions.select(),
+            thread_ts=self.state['thread'],
+        )
+        self.state['state'] = 'question'
+        self.state['last_victim_message'] = datetime.utcnow().timestamp()
+        save_file(CACHE_DIR / 'state.yaml', self.state)
+        print('Question answered')
+
+    async def timed_questions(self, client):
+        now = datetime.utcnow().timestamp()
+        last_victim_message = int(self.state.get('last_victim_message', 0))
+
+        if self.state.get('state', 'question') == 'question':
+            if now - last_victim_message > int(self.config.get('messages_delay')):
+
+                self.state['state'] = 'answer'
+                save_file(CACHE_DIR / 'state.yaml', self.state)
+                questions = RandomChoicer('questions.txt', base_dir=DATA_DIR, cache_dir=CACHE_DIR)
+                await client.chat_postMessage(
+                    channel=self.config['channel'],
+                    text=questions.select(),
+                    thread_ts=self.state['thread'],
+                )
+                print('Question asked')
+
+        await asyncio.sleep(5)
+        create_task(self.timed_questions(client))
+
+
 def main(client):
     config = load_file(DATA_DIR / 'config.yaml')
-    questions = RandomChoicer('questions.txt', base_dir=DATA_DIR, cache_dir=CACHE_DIR)
 
     try:
         state = load_file(CACHE_DIR / 'state.yaml')
@@ -25,27 +101,37 @@ def main(client):
 
     if 'victim' not in state:
         victims = RandomChoicer('victims.txt', base_dir=DATA_DIR, cache_dir=CACHE_DIR)
-        victim = victims.select()
-        state['victim'] = victim
+        while True:
+            victim = victims.select()
+            victim_id = re.match('<@(\w*)\|.*>', victim)
+            if not victim_id:
+                logger.error(f'Bad victim format in registry: {victim}. Reselecting...')
+                continue
+            state['victim'] = victim
+            state['victim_id'] = victim_id.group(1)
+            break
 
     if 'thread' not in state:
         print('Generating thread')
-        message = client.chat_postMessage(
+        message = get_event_loop().run_until_complete(client.chat_postMessage(
             channel=config['channel'],
             text=config['messages']['greeting'].format(victim=state["victim"]),
-        )
+        ))
         state['thread'] = message['ts']
 
     if 'last_victim_message' not in state:
-        state['last_victim_message'] = datetime.fromtimestamp(0)
+        state['last_victim_message'] = 0
 
     save_file(CACHE_DIR / 'state.yaml', state)
 
-    client.chat_postMessage(
-        channel=config['channel'],
-        text=questions.select(),
-        thread_ts=state['thread'],
-    )
+    bot_obj = IntroducerBot(state=state, config=config)
+
+    client_2 = IntroducerRTMClient(token=os.environ['SLACK_API_TOKEN'], run_async=True)
+    client_2.register('message', bot_obj.handle_message)
+
+    loop = get_event_loop()
+    ensure_future(bot_obj.timed_questions(client))
+    loop.run_until_complete(client_2.start())
 
 
 def load_file(filename):
@@ -59,4 +145,4 @@ def save_file(filename, data):
 
 
 if __name__ == '__main__':
-    main(get_client())
+    main(get_client(run_async=True))
